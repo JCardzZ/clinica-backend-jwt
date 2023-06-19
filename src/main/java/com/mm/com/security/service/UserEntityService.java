a224f0e92ca7c934420b7975e99d747a2b782035
Manuel de Jesús Cardoza Cardoza <mdcardoz@bancoagricola.com.sv>
2023-06-19 07:02:42 -0600

    Enlace de video de evidencia de funcionamineto de la App mas .zip de front y back como tambien de Postman collection

--- src/main/java/com/mm/com/security/service/UserEntityService.java
+++ src/main/java/com/mm/com/security/service/UserEntityService.java
@@ -16,15 +16,12 @@ import com.mm.com.security.jwt.JwtProvider;
 import com.mm.com.security.repository.UserEntityRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.Arrays;
 import java.util.List;
